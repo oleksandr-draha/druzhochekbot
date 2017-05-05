5a1f5ed2f7d44f80d2f72fe98b9094e00ec7b70e
Aleksandr Draga <a_draga@wargaming.net>
2017-05-05 12:36:23 +0300

    Catch exceptions and run bot forever

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 from time import sleep
+from traceback import format_exc
 
 from config.config import config
 from telegram.worker import TelegramWorker
@@ -8,6 +9,9 @@ bot = TelegramWorker()
 
 
 while not bot.stopped:
-    bot.process_messages()
-    bot.process_game_updates()
-    sleep(config.process_check_interval)
+    try:
+        bot.process_messages()
+        bot.process_game_updates()
+        sleep(config.process_check_interval)
+    except Exception, e:
+        bot.telegram_driver.admin_message(format_exc())
