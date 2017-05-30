a6f3af8fc2dbbe52b264787ff5de2d611beaa2cb
Aleksandr Draga <5408879@gmail.com>
2017-05-30 19:11:12 +0300

    Send uncommitted files.

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from time import sleep
 from traceback import format_exc
 
 from config.config import config
+from config.timeouts import timeouts
 from telegram.worker import TelegramWorker
 
 bot = TelegramWorker()
@@ -12,7 +13,7 @@ while not bot.stopped:
     try:
         bot.process_messages()
         bot.process_game_updates()
-        sleep(config.process_check_interval)
+        sleep(timeouts.process_check_interval)
     except Exception, e:
         bot.admin_message(format_exc())
         config.log_error(format_exc())
