881efbbc5c991c3c8317c27a3a9dfe37707d71a4
Aleksandr Draga <a_draga@wargaming.net>
2017-07-13 09:08:15 +0300

    Remove debug message

--- main.py
+++ main.py
@@ -2,8 +2,6 @@
 from time import sleep
 from traceback import format_exc
 
-from datetime import datetime
-
 from config.config import config
 from telegram.worker import TelegramWorker
 
@@ -14,7 +12,6 @@ while not bot.stopped:
     try:
         bot.process_messages()
         bot.process_game_updates()
-        bot.admin_message(str(datetime.now()))
         sleep(config.process_check_interval)
     except Exception, e:
         bot.admin_message(format_exc())
